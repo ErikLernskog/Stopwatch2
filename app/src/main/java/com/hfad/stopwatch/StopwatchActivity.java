6b56189f51bf0c70c575f672cadf57c3be9019db
ErikLernskog <funbo@hotmail.com>
2017-01-26 06:32:54 +0100

    handle rotation handle partly visible

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 public class StopwatchActivity extends Activity {
     private int seconds = 0;
     private boolean running;
-
+    private boolean wasRunning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,16 +17,42 @@ public class StopwatchActivity extends Activity {
         if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
     // Start the stopwatch running when the Start button is clicked
     public void onClickStart(View view) {
         running = true;
